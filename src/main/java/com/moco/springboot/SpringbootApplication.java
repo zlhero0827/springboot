ee34929dfd0408e39c66e8c808640af9c74a6a7f
leizhu27 <leizhu27@creditease.cn>
2018-08-14 14:04:52 +0800

    启动报错+数据库映射

--- src/main/java/com/moco/springboot/SpringbootApplication.java
+++ src/main/java/com/moco/springboot/SpringbootApplication.java
@@ -1,13 +1,10 @@
 package com.moco.springboot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@MapperScan("com.moco.springboot.controller")
+@SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/moco/springboot/mapper/UserSqlProvider.java
@@ -0,0 +1,29 @@
+package com.moco.springboot.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.jdbc.SQL;
+
+public class UserSqlProvider {
+    /**
+     * 方式1：在工具类的方法里,可以自己手工编写SQL。
+     */
+    public String listByUsername(String username) {
+        return "select * from t_user where username =#{username}";
+    }
+
+    /**
+     * 方式2：也可以根据官方提供的API来编写动态SQL。
+     */
+    public String getBadUser(@Param("username") String username, @Param("password") String password) {
+        return new SQL() {{
+            SELECT("*");
+            FROM("t_user");
+            if (username != null && password != null) {
+                WHERE("username like #{username} and password like #{password}");
+            } else {
+                WHERE("1=2");
+            }
+        }}.toString();
+    }
+
+}
